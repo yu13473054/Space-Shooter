0b25c9a9bed0a9f71304881834c07a75bf805839
yu13473054 <849460181@qq.com>
2017-04-25 23:10:06 +0800

    添加飞船背景，移动飞船

---
+++ Assets/_Scripts/PlayerControl.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+	public float speed;
+	private Rigidbody playerRb;
+
+	void Start(){
+		playerRb = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate(){
+		float horf = Input.GetAxis ("Horizontal");
+		float verf = Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3 (horf, 0, verf);
+		playerRb.velocity = movement*speed;
+	}
+}
