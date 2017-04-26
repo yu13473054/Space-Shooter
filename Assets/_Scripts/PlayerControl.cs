c8f38eae09e2eaa7106651d8e62725a3f594ecef
yu13473054 <849460181@qq.com>
2017-04-26 22:33:46 +0800

    增加飞机边界限定，移动过程中添加侧翻效果

--- Assets/_Scripts/PlayerControl.cs
+++ Assets/_Scripts/PlayerControl.cs
@@ -1,18 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerControl : MonoBehaviour {
-	public float speed;
-	private Rigidbody playerRb;
+[System.Serializable]
+public class Boundary
+{
+	public float xMin, xMax, zMin, zMax;
+}
 
-	void Start(){
-		playerRb = GetComponent<Rigidbody>();
+public class PlayerControl : MonoBehaviour
+{
+	private Rigidbody playerRb;
+	public float speed;
+	public Boundary boundary;
+	public float tilt;
+
+	void Start ()
+	{
+		playerRb = GetComponent<Rigidbody> ();
 	}
 
-	void FixedUpdate(){
+	void FixedUpdate ()
+	{
 		float horf = Input.GetAxis ("Horizontal");
 		float verf = Input.GetAxis ("Vertical");
 		Vector3 movement = new Vector3 (horf, 0, verf);
-		playerRb.velocity = movement*speed;
+		playerRb.velocity = movement * speed;
+
+		playerRb.position = new Vector3 (
+			Mathf.Clamp (playerRb.position.x, boundary.xMin, boundary.xMax), 
+			0, 
+			Mathf.Clamp (playerRb.position.z, boundary.zMin, boundary.zMax)
+		);
+
+		playerRb.rotation = Quaternion.Euler (0, 0, playerRb.velocity.x * -tilt);
 	}
 }
