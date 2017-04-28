54ba5a401b764b824d6bd0fe9fb81797efc53272
yu13473054 <849460181@qq.com>
2017-04-28 16:31:23 +0800

    重新添加远程仓库的文件

---
+++ Assets/_Scripts/PlayerControl.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary
+{
+	public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerControl : MonoBehaviour
+{
+	private Rigidbody playerRb;
+	private float nextFire;
+
+	public float speed;
+	public Boundary boundary;
+	public float tilt;
+	public GameObject shot;
+	public Transform spotSpawn;
+	public float fireRate ;
+
+	void Start ()
+	{
+		playerRb = GetComponent<Rigidbody> ();
+	}
+
+	void Update(){
+		if(Input.GetButton("Fire1") && Time.time>nextFire){
+			nextFire = Time.time + fireRate;
+			Instantiate (shot,spotSpawn.position,spotSpawn.rotation);
+		}
+	}
+
+
+	void FixedUpdate ()
+	{
+		float horf = Input.GetAxis ("Horizontal");
+		float verf = Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3 (horf, 0, verf);
+		playerRb.velocity = movement * speed;
+
+		playerRb.position = new Vector3 (
+			Mathf.Clamp (playerRb.position.x, boundary.xMin, boundary.xMax), 
+			0, 
+			Mathf.Clamp (playerRb.position.z, boundary.zMin, boundary.zMax)
+		);
+
+		playerRb.rotation = Quaternion.Euler (0, 0, playerRb.velocity.x * -tilt);
+	}
+}
